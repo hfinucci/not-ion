d49c95e64123231be47b04af8802e545e3c9f4ba
Azul <azkim@itba.edu.ar>
2022-11-30 09:05:21 -0300

    add swagger documentation
    To view the documentation, go to /docs

--- index.ts
+++ index.ts
@@ -1,44 +1,47 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Express, Request, Response } from "express";
 import DashboardPersistence from "./database/dashboardPersistence";
 import validTypes from "./utils";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "./swagger.json";
 
-const app: Express = express()
-const port = 8000
-const bodyParser = require('body-parser');
-app.use(bodyParser.json())
+const app: Express = express();
+const port = 8000;
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
+app.post("/dashboard", async (req, res) => {
+  await DashboardPersistence.createDashboard();
+  res.sendStatus(200);
+});
 
-app.post('/dashboard', async (req, res) => {
-    await DashboardPersistence.createDashboard()
-    res.sendStatus(200)
-})
+app.get("/dashboard", async (req, res) => {
+  const result = await DashboardPersistence.getDashboard();
+  res.send(result);
+});
 
-app.get('/dashboard', async (req, res) => {
-    const result = await DashboardPersistence.getDashboard()
-    res.send(result)
-})
+app.post("/blocks", async (req, res) => {
+  // TODO manejo de logica con mongo
+  if (validTypes.includes(req.body.type)) {
+    await DashboardPersistence.incrementValue(req.body.type, 1);
+  } else {
+    res.sendStatus(400);
+    return;
+  }
+  res.sendStatus(200);
+});
 
-app.post('/blocks', async (req, res) => {
-    // TODO manejo de logica con mongo
-    if (validTypes.includes(req.body.type)) {
-        await DashboardPersistence.incrementValue(req.body.type, 1)
-    } else {
-        res.sendStatus(400)
-        return
-    }
-    res.sendStatus(200)
-})
+app.delete("/blocks", async (req, res) => {
+  if (validTypes.includes(req.body.type)) {
+    await DashboardPersistence.incrementValue(req.body.type, -1);
+  } else {
+    res.sendStatus(400);
+    return;
+  }
+  res.sendStatus(200);
+});
 
-app.delete('/blocks', async (req, res) => {
-    if (validTypes.includes(req.body.type)) {
-        await DashboardPersistence.incrementValue(req.body.type, -1)
-    } else {
-        res.sendStatus(400)
-        return
-    }
-    res.sendStatus(200)
-})
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
-    console.log('listening on port 8000...')
-})
+  console.log("listening on port 8000...");
+});
